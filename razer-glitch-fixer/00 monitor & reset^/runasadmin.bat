ebb3dcefc15f00825f95f66a805835218be3d9f5
workturnedplay <workturnedplay+gitcommits@gmail.com>
2026-07-19 07:37:21 +0200

    use NoDefaultCurrentDirectoryInExePath
    via Gemini 3.5 Thinking

--- razer-glitch-fixer/00 monitor & reset^/runasadmin.bat
+++ razer-glitch-fixer/00 monitor & reset^/runasadmin.bat
@@ -1,4 +1,6 @@
 @echo off
+rem 1. Prevent the current working directory from taking precedence over PATH, doesn't work with eg. "start go.exe"
+set "NoDefaultCurrentDirectoryInExePath=1"
 
 :: (nope:)disallow Ctrl+break, no effect, it still prompts: Terminate batch job (Y/N)?
 ::break off
--- spy--/build.bat
+++ spy--/build.bat
@@ -1,4 +1,6 @@
 @echo off
+rem 1. Prevent the current working directory from taking precedence over PATH, doesn't work with eg. "start go.exe"
+set "NoDefaultCurrentDirectoryInExePath=1"
 
 ::if running as admin must get back to current dir:
 cd /d %~dp0
